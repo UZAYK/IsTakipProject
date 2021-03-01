119d61a04b48cf64b8d05c67df1bf0ec37c05eab
UZAY KAHRAMAN <uzaykahramann@gmail.com>
2021-03-01 21:28:50 +0300

    Son Guncel

---
+++ IsTakip.Web/wwwroot/js/SignInJs.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IsTakip.Web.wwwroot.js
+{
+    public class SignInJs
+    {
+    }
+}
